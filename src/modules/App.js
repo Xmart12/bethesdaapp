8c185c119cf8e84ceb75f08f928ba72e34036c8b
Mario Espinoza <mario.espinoza@dada-dada.com>
2020-02-08 16:22:44 -0600

    Continuacion de desarrollo

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
-import { Drawer, AppBar, Toolbar, IconButton, CssBaseline } from '@material-ui/core';
-import { Menu } from '@material-ui/icons';
+import { Router, Route, Link } from 'react-router-dom';
+import { Drawer, AppBar, Toolbar, IconButton, CssBaseline, Divider, List, ListItem, ListItemText } from '@material-ui/core';
+import { Menu, ChevronRight, ChevronLeft } from '@material-ui/icons';
 import { useStyles, useTheme } from '../dependences/material';
 
 import logo from './logo.svg';
 import './App.css';
-
+import Movements from './movements/Movements';
+import Test from './test/test';
 
 
 function App() {
@@ -21,7 +23,15 @@ function App() {
 
   const handleDrawerClose = () => {
     setOpen(false);
-  }
+  };
+
+  const [drawer, setDrawer] = useState(false);
+  const [title, setTitle] = useState("Test");
+
+  const onItemClick = (title) => {
+    //setTitle(title);
+    //setDrawer(drawer);
+  };
 
   return (
     <div className="App">
@@ -38,8 +48,37 @@ function App() {
         </Toolbar>
       </AppBar>
 
-      
+      <Router>
 
+        <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{ paper: classes.drawerPaper }}>
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              { theme.direction === 'ltr' ? <ChevronLeft /> : <ChevronRight /> }
+            </IconButton>
+            <Divider/>
+            <List>
+
+              <ListItem button component={Link} to="/" onClick={onItemClick("Test")}>
+                <ListItemText>Home</ListItemText>
+              </ListItem>
+              <ListItem button component={Link} to="/Movements" onClick={onItemClick("Movements")}>
+                <ListItemText>Page 1</ListItemText>
+              </ListItem>
+
+            </List>
+          </div>
+        </Drawer>
+
+        <main className={clsx(classes.content, { [classes.contentShift]: open })}>
+
+        <div className={classes.drawerHeader} />
+
+        <Route exact path="/" component={Test} />
+        <Route path="/Movements" component={Movements} />
+
+      </main>
+
+      </Router>
     </div>
   );
 }
---
+++ src/modules/test/test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Test extends React.Component {
+
+    render () {
+
+        return (
+
+        <React.Fragment>
+            <h1>Holo</h1>
+        </React.Fragment>
+        
+        )
+    }
+
+}
+
+export default Test;
